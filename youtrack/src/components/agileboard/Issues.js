f98a472317c5dd35bc63829718bf9f6e8c31aa6b
Wazeback <david.lelystad@gmail.com>
2022-11-24 14:15:49 +0100

    added database connection

--- youtrack/src/components/agileboard/Issues.js
+++ youtrack/src/components/agileboard/Issues.js
@@ -4,24 +4,23 @@ import { IssuesHeader } from './IssuesHeader';
 import { IssuesNavbar } from './IssuesNavbar';
 import { IssuesCreate } from './IssuesCreate';
 import { IssuesNestling } from './IssuesNestling';
-import { IssuesData } from './IssuesData';
 
 export default class Issues extends React.Component {
 
     state = {
-        issues: []
+        sprintData: []
     }
 
     componentDidMount() {
-        axios.get(`http://scuffedutrackbackend.test/api/issues`)
+        axios.get(`http://scuffedutrackbackend.test/api/sprintIssues`)
             .then(res => {
-                const issues = res.data;
-                this.setState({ issues });
+                const sprintData = res.data;
+                this.setState({ sprintData });
             })
     }
 
     render() {
-        console.log(this.state.issues);
+        // console.log(this.state.sprint);
         return (
 
             <div className='flex flex-col text-white lg:px-[150px] px-3 py-5 text-[#D5D8DF]'>
@@ -32,44 +31,42 @@ export default class Issues extends React.Component {
 
                     <IssuesNavbar/>
 
-                    {this.state.issues.issues ? this.state.issues.issues.map((Data, i) => {
-                        console.log(Data)
-                        return (
-                            <>
-                                <div className='flex border border-gray-600 bg-[#30324E]'>
-                                    <div className='border-r border-gray-600 px-3 py-3'>
-                                        <div className='border border-gray-600 h-full w-[24px]'/>
-                                    </div>
-                                    <div className='border-r border-gray-600 w-[35%] px-3 py-3'>
-                                        <h4>{Data.title}</h4>
-                                    </div>
-                                    <div className='flex justify-center border-r border-gray-600 w-[18%] px-3 py-3'>
-                                        <div className='border border-gray-600 h-full w-[24px] rounded-full'>
-                                            <h4>{Data.id}</h4>
+                    {this.state.sprintData.sprint ? this.state.sprintData.sprint.map((Data, i) => {
+
+                            // console.log(issue);
+
+                            return (
+                                <>
+                                {Data.issues.map((issue, i) => {
+                                    console.log(issue);
+                                    return(
+                                    <div className='flex border border-gray-600 bg-[#30324E]'>
+                                        <div className='border-r border-gray-600 px-3 py-3'>
+                                            <div className='border border-gray-600 h-full w-[24px]'/>
                                         </div>
+                                        <div className='border-r border-gray-600 w-[35%] px-3 py-3'>
+                                            <h4>{issue.title}</h4>
+                                        </div>
+                                        <div className='flex justify-center border-r border-gray-600 w-[18%] px-3 py-3'>
+                                            <div className='border border-gray-600 h-full w-[24px] rounded-full'>
+                                                <h4>{issue.user_id}</h4>
+                                            </div>
+                                        </div>
+                                        <div className='border-r border-gray-600 px-3 py-3'>
+                                            <h4>{issue.priority.priorities}</h4>
+                                        </div>
+                                        <div className='flex justify-center border-r border-gray-600 w-[18%] px-3 py-3'>
+                                            <h4>{issue.subject.subject}</h4>
+                                        </div>
+                                        <div className='border-gray-600 w-full px-3 py-3 lg:block hidden'/>
                                     </div>
-                                    <select className='border-r border-gray-600 w-[28%] px-3 py-3'>
-                                        {/*{Data.description.map((Status, i) => {*/}
-                                        {/*    return (*/}
-                                        {/*        <>*/}
-                                        {/*            <option>{Status}</option>*/}
-                                        {/*        </>*/}
-                                        {/*    )*/}
-                                        {/*})}*/}\
-                                        <h4>{Data.description}</h4>
-                                    </select>
-                                    <select className='border-r border-gray-600 w-[28%] px-3 py-3'>
-                                        {/*{Data.id.map((Subject, i) => {*/}
-                                        {/*    return (*/}
-                                        {/*        <>*/}
-                                        {/*            <option value={Subject}>{Subject}</option>*/}
-                                        {/*        </>*/}
-                                        {/*    )*/}
-                                        {/*})}*/}
-                                    </select>
-                                    <div className='border-gray-600 w-full px-3 py-3 lg:block hidden'/>
-                                </div>                            </>
-                        )
+                                    )
+                                })}
+
+                                </>
+
+                            )
+
                     }): null}
 
                     {/*<IssuesNestling/>*/}
