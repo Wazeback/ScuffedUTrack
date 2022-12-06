8f03cd70a8faf8b30dc25316468a6a98c9b49d0b
Wazeback <david.lelystad@gmail.com>
2022-12-06 23:44:05 +0100

    fixed bugs

--- src/components/groups/CreateGroupMenu.js
+++ src/components/groups/CreateGroupMenu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from "axios";
 import Select from 'react-select'
-import makeAnimated from 'react-select/animated';
+
 
 export default class CreateGroupMenu extends React.Component {
     constructor(props) {
@@ -9,42 +9,41 @@ export default class CreateGroupMenu extends React.Component {
 
         this.state = {
             name: '',
-            user_id: [],
+            id: [],
             start: '',
             end: '',
-            GroupData: []
+            GroupData: [],
+            selectOptions : [],
         }
 
         this.name = this.name.bind(this);
-        this.user_id = this.user_id.bind(this);
 
         const options = [
             { value: 'chocolate', label: 'Chocolate' },
             { value: 'strawberry', label: 'Strawberry' },
             { value: 'vanilla', label: 'Vanilla' }
         ]
-
-
-
-        const MyComponent = () => (
-            <Select options={options} />
-        )
-
     }
-
-
 
     closeMenu = (e) => {
         this.props.changeHandler(e);
     }
 
-
+    handleChange(e){
+        this.setState({id:e.value})
+    }
 
     componentDidMount() {
         axios.get(`http://127.0.0.1:8000/api/yearUsers`)
             .then(res => {
                 const GroupData = res.data;
                 this.setState({ GroupData });
+                const options = GroupData.users.map(user => ({
+                    "value" : user.id,
+                    "label" : user.name
+                }))
+                this.setState({selectOptions: options})
+                // console.log(this.state.selectOptions)
             })
     }
 
@@ -59,7 +58,7 @@ export default class CreateGroupMenu extends React.Component {
             user_id: this.state.user_id,
         };
 
-        console.log(packets);
+        // console.log(this.state.selectOptions);
 
         axios.post('http://127.0.0.1:8000/api/group/create', packets)
             .then(
@@ -74,25 +73,22 @@ export default class CreateGroupMenu extends React.Component {
 
 
 
+
     render() {
-        // {console.log(this.state.GroupData)}
         return (
             <>
                 <form className='absolute max-w-[520px] flex flex-row bg-light rounded-md'>
                     <div className='flex flex-col px-2 py-2.5'>
                         <span className='w-3/5 text-color'>Name:</span>
                         <input type="text" name="name" onChange={this.name} value={this.state.name} className='w-full h-[24px] mb-3 rounded-md text-color bg-[#4b4e69] border border-color outline-none'/>
-                        <span className='w-3/5 text-color'>Users:</span>
-                        <div className='flex items-center text-sm text-color'>
+                        <span className='w-3/5 text-color'>Please select some users:</span>
+                        <div className='flex items-center w-[350px] text-sm'>
                             <div className="w-full">
-                                <label className="inline-block text-sm text-gray-600" htmlFor="Multiselect">Select multiple roles</label>
-                                <Select
-                                    closeMenuOnSelect={false}
-                                    components={animatedComponents}
-                                    defaultValue={["asd", "asdas"]}
-                                    isMulti
-                                    options={"sadas"}
-                                />
+                                <div>
+                                    <Select isMulti 
+                                        options={this.state.selectOptions}
+                                        onChange={this.handleChange.bind(this)} />
+                                </div>
                             </div>
                             <button type="submit" onClick={this.handleSubmit} className='rounded-md border ml-2  border-color px-1 py-1 text-color bg-[#4b4e69] outline-none'>Create</button>
                         </div>
